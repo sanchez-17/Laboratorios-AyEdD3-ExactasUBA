12257df60066fd761c84c17dea8eaeffa20b8539
Gaston Sanchez <sanchez-17@outlook.com>
2012-01-01 05:48:38 -0300

    Ej.2 TP1 Solved

--- Practica 1/TP1 Tecnicas Algoritmicas/Ej2.cpp
+++ Practica 1/TP1 Tecnicas Algoritmicas/Ej2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include "../backtracking/utilities.h"
-#define ll long long
+#define ll unsigned long long
 using namespace std;
 int N;vector<vector<ll>> M;ll MOD = 1e9 + 7;
-ll SUM_MIN=1e9;
+ll SUM_MIN=1e10;
 
 void f(vector<bool>& usados,ll descGral,ll tiempo,int cnt){
     if(cnt==N) SUM_MIN = descGral < SUM_MIN ? descGral : SUM_MIN;
@@ -11,7 +11,7 @@ void f(vector<bool>& usados,ll descGral,ll tiempo,int cnt){
         forn(i,N) if(!usados[i]){
             usados[i] = true;
             ll tiempoActual =  tiempo+M[0][i];
-            f(usados, ( descGral % MOD +  (tiempoActual * M[1][i]) % MOD ) % MOD,tiempoActual, cnt+1);
+            f(usados, descGral +  tiempoActual * M[1][i],tiempoActual, cnt+1);
             usados[i] = false;
         }
     }
@@ -20,17 +20,17 @@ void f(vector<bool>& usados,ll descGral,ll tiempo,int cnt){
 void solve(){
     vector<bool> usados(N,false);
     f(usados,0,0,0);
-    cout<<SUM_MIN<<"\n";
+    cout<<SUM_MIN % MOD<<"\n";
 }
 
 int main(){
     int m;cin>>m;
     forn(i,m) {
         cin >> N; M.clear(); M.rz(2,vector<ll>(N));
-        forn(i,2) forn(j,N) cin >> M[i][j];
+        forn(j,2) forn(k,N) cin >> M[j][k];
         impMatriz(M);
         solve();
-        SUM_MIN = 1e9;
+        SUM_MIN = 1e10;
     }
 
     return 0;
