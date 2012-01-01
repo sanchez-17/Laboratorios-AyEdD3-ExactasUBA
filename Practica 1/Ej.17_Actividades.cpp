f68eca9c767c79956dce360c53772f0083328ba6
Gaston Sanchez <sanchez-17@outlook.com>
2012-01-01 00:11:44 -0300

    Ej.8 P1 Vara. Se toma en cuenta el rango valido del corte

---
+++ Practica 1/Ej.17_Actividades.cpp
@@ -0,0 +1,14 @@
+#include "../../backtracking/utilities.h"
+int N;vector<int> numbers;
+//Estrategia greedy: Elegir la actividad que termine antes.
+void solve(vector<int>& numbers,int i){
+	|||}
+
+int main(){
+	cin>>N;numbers.rz(N);
+	forn(i,N) cin >> numbers[i];
+	sort(numbers.begin(),numbers.end());
+	solve(numbers);
+	return 0;
+}
+
--- Practica 1/Greedy/Ej14_sumaElem.cpp
+++ Practica 1/Greedy/Ej14_sumaElem.cpp
@@ -1,20 +1,25 @@
-#include "../../backtracking/utilities.h"
-#include <queue>
+#include "../backtracking/utilities.h"
+#include <algorithm>
 int N;vector<int> numbers;
 //Estrategia greedy: Ordenar de menor a mayor y empezar a sumar
-void solve(vector<int>& numbers,int i){
+void solve(){
 	int sum = 0;
 	int costo = 0;
 	for(int n:numbers){
-		sum += d
+		sum += n;
+		costo += sum;
 	}
+	cout<<costo<<endl;
 	
 }
+
+//C={1,2,5} => 1 + (1+2) + (3+5) = 12
+//C={3,5,1} => 3 + (3+5) + (8+1) = 20
 
 int main(){
 	cin>>N;numbers.rz(N);
 	forn(i,N) cin >> numbers[i];
 	sort(numbers.begin(),numbers.end());
-	solve(numbers);
+	solve();
 	return 0;
 }
