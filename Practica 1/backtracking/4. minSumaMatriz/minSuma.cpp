fc78747a704e278d61d43a21b9c17ce11b787611
Sanchez Gaston <sanchez-17@outlook.com>
2023-08-21 01:07:54 -0300

    test añadido a sumMin Ej4.P1

--- Practica 1/backtracking/4. minSumaMatriz/minSuma.cpp
+++ Practica 1/backtracking/4. minSumaMatriz/minSuma.cpp
@@ -9,49 +9,45 @@ using namespace std;
 #define forr(i,a,b) for(int i=(int)a;i<(int)b;i++)
 #define forn(i,n) forr(i,0,n)
 #define matriz vector<vector<int>>
-matriz D;int SUMA,N;vector<int> res;
-//En res guardare la permutacion que genera la min suma. No anda =(
+//Variables globales
+matriz M;int SUMA=1e7,N;vector<int> RES;
+//En res guardare la permutacion que genera la min suma. En caso
+//de haber mas de una permutacion posible, se queda con la primera.
 //La permutacion que extiende a I, usando todos los elementos de 1 a n, y
 //que la sumatoria S(ver apunte) sea minima.
 
-int suma(matriz& M,vector<int>& I){
+void suma(vector<int>& I){
 	int sum = 0;
 	forn(i,N) sum += M[ I[i] ][ I[(i+1)%N] ];
-	return sum;
+	if(sum < SUMA){ SUMA = sum; RES = I;}
 }
 
 
-void minSuma(matriz& M, vector<bool>& usado,vector<int>& I){
-	if(sz(M)==N){//Si ya tengo una permutacion de {1...N}
-		int sum = suma(M,I);
-		if(sum <= SUMA){
-			SUMA = sum; res = I;
-		}
+void minSuma(vector<bool>& usado,vector<int>& I){
+	if(sz(I)==N){//Si ya tengo una permutacion de {1...N}
+		suma(I);	
 	}else{
 		forn(i,N) if (!usado[i]){
 				usado[i] = true; I.pb(i);
-				minSuma(M,usado,I);
+				minSuma(usado,I);
 				usado[i] = false; I.ppb();
 			}
 		}
 }
 
-void solve(matriz& M){
+void solve(){
 	vector<bool> usado(N,false);
-	//usado[0] = true;
 	//Inicializo I como pi(i)=i. Para poder comenzar a comparar las sumas
-	vector<int> I; forn(i,N) I.pb(i); res = I;
-	impVector(res);
-	SUMA = suma(M,I); I.clear();
-	minSuma(M,usado,I);
+	vector<int> I;
+	minSuma(usado,I);
 	cout << SUMA << "\n";
-	impVector(res);
+	impVector(RES);
 
 }
 
 int main(){
-	cin >> N;D.rz(N,vector<int>(N));
-	forn(i,N) forn(j,N) cin >> D[i][j];
-	solve(D);
+	cin >> N;M.rz(N,vector<int>(N));
+	forn(i,N) forn(j,N) cin >> M[i][j];
+	solve();
 	return 0;
 }
