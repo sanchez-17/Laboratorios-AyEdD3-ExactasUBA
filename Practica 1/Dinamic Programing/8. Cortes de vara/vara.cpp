9a779187eee445e2db5b92c4489769397df68808
Gaston Sanchez <sanchez-17@outlook.com>
2023-08-30 17:29:51 -0300

    Ej vara

--- Practica 1/Dinamic Programing/8. Cortes de vara/vara.cpp
+++ Practica 1/Dinamic Programing/8. Cortes de vara/vara.cpp
@@ -3,12 +3,15 @@ int N,L;
 vector<int> C;vector<bool> usado;
 
 int s(int i,int j, int cnt ){
-	if(cnt==N) return j-i;
+	if(cnt==N) return 0;
 	else {
 		int min_coste = 1e7;
+		
 		forn(k,N) if(!usado[k]){
+			
 			usado[k]=true;
 			int coste_k = s(i,C[k],cnt+1) + s(C[k],j,cnt+1);
+			cout<<"i:"<<i<<" j:"<<j<<" c: "<<C[k]<<" minCosto:" <<min_coste<<" coste_k:"<<coste_k<<endl;
 			min_coste = coste_k < min_coste ? coste_k : min_coste;
 			usado[k]=false;
 		}
