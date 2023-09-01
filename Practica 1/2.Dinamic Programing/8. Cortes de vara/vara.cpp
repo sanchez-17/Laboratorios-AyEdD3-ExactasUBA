3c3e46cce79910dadc5dfe1b311f64ab223084e7
Gaston Sanchez <sanchez-17@outlook.com>
2023-09-01 16:55:25 -0300

    P1.Ej8

--- Practica 1/2.Dinamic Programing/8. Cortes de vara/vara.cpp
+++ Practica 1/2.Dinamic Programing/8. Cortes de vara/vara.cpp
@@ -2,14 +2,20 @@
 int N,L,INF = 1e7;
 vector<int> C;vector<bool> usado;
 
+bool noHayMasCortes(int i, int j){
+    bool res = true;
+    forr(k, i+1, j) res = res && usado[k];
+    return res;
+}
+
 int s(int i,int j, int cnt ){
-	if(cnt==N) return 0;
+	if(cnt == N && noHayMasCortes(i,j)) {cout << "Caso Base:";DBG(usado);return 0;}
 	else {
-		//DBG(usado);
+		DBG(usado);
 		int min_coste = INF;
 		//Falta chequear que los cortes k esten dentro del reango de i,j
 		forn(k,N) if(!usado[k] && i<C[k] && C[k]<j){
-			//usado[k]=true;
+			usado[k]=true;
 			int coste_k = s(i,C[k],cnt+1) + s(C[k],j,cnt+1);
 			min_coste = coste_k < min_coste ? coste_k : min_coste;
 			//DBG4(i,j,C[k],coste_k);
@@ -18,7 +24,6 @@ int s(int i,int j, int cnt ){
 		//DBGY(cnt);DBG(min_coste);RAYA;
 		return (j-i) + min_coste;
 	}
-		
 }
 
 int main(){
---
+++ Practica 1/2.Dinamic Programing/9. Vida/vida.cpp
@@ -0,0 +1,5 @@
+int N,M;
+
+int f(int i, int j){
+	if()
+}
